70458691a569410d72e492dcaea23d4ac1fb41e2
andmicbro <andmicbro@gmail.com>
2013-11-08 03:05:56 -0700

    Alright guys, here's hoping this works.

---
+++ Hangman/src/hangmanPkg/HangmanGUI.java
@@ -0,0 +1,91 @@
+package hangmanPkg;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+import java.awt.Font;
+import java.awt.Color;
+
+public class HangmanGUI extends JFrame 
+{
+
+	private JPanel hangmanPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
+					HangmanGUI frame = new HangmanGUI();
+					frame.setVisible(true);
+				} catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HangmanGUI() 
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 400);			// I increased the window size here. We can play around with it as needed.
+		hangmanPane = new JPanel();
+		hangmanPane.setBackground(new Color(240, 240, 240));
+		hangmanPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		hangmanPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(hangmanPane);
+		
+		JPanel titlePanel = newTitlePanel();
+		hangmanPane.add(titlePanel, BorderLayout.NORTH);
+		
+		JPanel hangmanPanel = newHangmanPanel();
+		hangmanPane.add(hangmanPanel, BorderLayout.EAST);
+	}
+	
+	// This is the title panel.
+	
+	private JPanel newTitlePanel() 
+	{	
+		JPanel titlePanel = new JPanel();
+		titlePanel.setBorder(new EmptyBorder(5,5,5,5));
+		
+		JLabel lblHangman = new JLabel("Hangman");
+		lblHangman.setFont(new Font("Kristen ITC", Font.ITALIC, 30));
+		titlePanel.add(lblHangman);
+		return titlePanel;
+	}
+	
+	// This panel will contain everything for our hangman. There isn't much here yet, but we'll add as we go. 
+	
+	private JPanel newHangmanPanel() 
+	{	
+		JPanel hangmanPanel = new JPanel();
+		{
+			JLabel lblHngManImageLabel = new JLabel(" ");
+			// This image is just a placeholder for now. We'll need to add all the parts of the hangman as user
+			// user input is obtained. The initail gallows image will probably go here though.
+			lblHngManImageLabel.setIcon(new ImageIcon(HangmanGUI.class.getResource("1-Hangman.jpg")));
+			hangmanPanel.add(lblHngManImageLabel);
+
+		}
+		// TODO We need to make the methods for this panel. 
+		return hangmanPanel;
+	}
+
+}
