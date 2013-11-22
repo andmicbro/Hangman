c3be2da915ad8ed70aeac64d68de24b2178d0e61
Chris McAfee <Chris.mcafee@gmail.com>
2013-11-22 02:43:45 -0700

    Create GameRunnerApp.java
    This has main, but is really jsut a bunch of tests

---
+++ GameRunnerApp.java
@@ -0,0 +1,32 @@
+package hangmanPkg;
+
+public class GameRunnerApp {
+
+	public static void main(String[] args) {
+		GameRunner myGR = new GameRunner();
+		myGR.userGuess("e");
+		System.out.println(myGR.getLetters());
+
+		System.out.println(myGR.getWord());
+		System.out.println("guess: e");
+
+		System.out.println("wrong guesses: "+myGR.getWrongGuessCounter ());
+		System.out.println("index of correct guesses: "+myGR.getIndexOfCorrectGuess());
+		System.out.println("letters tried: "+myGR.getLettersTried());
+
+		myGR.userGuess("h");
+		System.out.println("guess: h");
+
+		System.out.println("wrong guesses: "+myGR.getWrongGuessCounter ());
+		System.out.println("index of correct guesses: "+myGR.getIndexOfCorrectGuess());
+		System.out.println("letters tried: "+myGR.getLettersTried());
+		myGR.userGuess("r");
+		System.out.println("guess: r");
+
+		System.out.println("wrong guesses: "+myGR.getWrongGuessCounter ());
+		System.out.println("index of correct guesses: "+myGR.getIndexOfCorrectGuess());
+		System.out.println("letters tried: "+myGR.getLettersTried());
+
+	}
+
+}
